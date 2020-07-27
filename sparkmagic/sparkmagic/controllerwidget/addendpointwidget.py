ac4c38b56d206405d3cf89757d785bd17e76aa90
alexism9 <alexism9@cs.washington.edu>
2020-07-27 11:13:35 -0700

    widget children array

--- sparkmagic/sparkmagic/controllerwidget/addendpointwidget.py
+++ sparkmagic/sparkmagic/controllerwidget/addendpointwidget.py
@@ -46,14 +46,22 @@ class AddEndpointWidget(AbstractMenuWidget):
         )
  
         self.auth_type.on_trait_change(self._update_auth)
-        
-        self.children = [self.ipywidget_factory.get_html(value="<br/>", width=widget_width), self.auth_type]
+        self.children = []
+        self.children.append(self.ipywidget_factory.get_html(value="<br/>", width=widget_width))
+        self.children.append(self.auth_type)
+        self.children.append(self.auth.get_widgets())
+        self.children.append(self.ipywidget_factory.get_html(value="<br/>", width=widget_width))
+        self.children.append(self.submit_widget)
+
+
+
+        #self.children = [self.ipywidget_factory.get_html(value="<br/>", width=widget_width), self.auth_type]
         #here we add the url widget from authenticator. I have this because google auth changes value of url widget depending 
         #on what cluster project and region they enter, but will probably move url widget to this class and just set its 
         #value in the google auth. This is primarily to see if this will work for other things like adding in the project 
         #id textbox for google. 
-        self.children = self.children.append(self.auth.get_widgets())
-        self.children = self.children.append([self.ipywidget_factory.get_html(value="<br/>", width=widget_width), self.submit_widget])
+    
+        #self.children = self.children.append([self.ipywidget_factory.get_html(value="<br/>", width=widget_width), self.submit_widget])
         
         for child in self.children:
             child.parent_widget = self
