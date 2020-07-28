fafcd4233d7a1b0136e903da041039b9012ec243
alexism9 <alexism9@cs.washington.edu>
2020-07-28 12:25:20 -0700

    add back in endpoint

--- sparkmagic/sparkmagic/livyclientlib/endpoint.py
+++ sparkmagic/sparkmagic/livyclientlib/endpoint.py
@@ -3,30 +3,30 @@ from sparkmagic.utils.constants import AUTHS_SUPPORTED
 
 
 class Endpoint(object):
-    def __init__(self, url, auth, implicitly_added=False):
+    def __init__(self, url, auth, username="", password="", implicitly_added=False):
         if not url:
             raise BadUserDataException(u"URL must not be empty")
         #if auth.login_service is not key in dictionary from auths in config file: 
-        #if auth not in AUTHS_SUPPORTED:
-        #    raise BadUserConfigurationException(u"Auth '{}' not supported".format(auth))
+        if auth not in AUTHS_SUPPORTED:
+            raise BadUserConfigurationException(u"Auth '{}' not supported".format(auth))
         
         self.url = url.rstrip(u"/")
+        self.username = username
+        self.password = password
         self.auth = auth
         # implicitly_added is set to True only if the endpoint wasn't configured manually by the user through
         # a widget, but was instead implicitly defined as an endpoint to a wrapper kernel in the configuration
         # JSON file.
         self.implicitly_added = implicitly_added
 
-    #I don't think we will need 
+
     def __eq__(self, other):
         if type(other) is not Endpoint:
             return False
-            #change self.auth to self.auth.login_service == other.auth.login_service
-        return self.url == other.url 
+        return self.url == other.url and self.username == other.username and self.password == other.password and self.auth == other.auth 
 
-    #change to self.auth.name
     def __hash__(self):
-        return hash((self.url, self.auth))
+        return hash((self.url, self.username, self.password, self.auth))
 
     def __ne__(self, other):
         return not self == other
