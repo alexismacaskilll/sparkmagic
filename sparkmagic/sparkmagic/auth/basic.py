2106d264df685bf7cfab00a043ebb9638ee43628
alexism9 <alexism9@cs.washington.edu>
2020-07-29 09:16:53 -0700

    add wiidth param

--- sparkmagic/sparkmagic/auth/basic.py
+++ sparkmagic/sparkmagic/auth/basic.py
@@ -19,7 +19,7 @@ class Basic(HTTPBasicAuth, Authenticator):
         
 
     def get_widgets(self, widget_width): 
-        Authenticator.get_widgets(self)
+        Authenticator.get_widgets(self, widget_width)
         ipywidget_factory = IpyWidgetFactory()
         
         """
