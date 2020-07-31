8bf4f6942a3628a47d8d74750882537dc7cbc0b6
alexism9 <alexism9@cs.washington.edu>
2020-07-31 10:02:43 -0700

    clean up white space

--- sparkmagic/sparkmagic/livyclientlib/endpoint.py
+++ sparkmagic/sparkmagic/livyclientlib/endpoint.py
@@ -4,18 +4,14 @@ class Endpoint(object):
     def __init__(self, url, auth, implicitly_added=False):
         if not url:
             raise BadUserDataException(u"URL must not be empty")
-        #if auth.login_service is not key in dictionary from auths in config file: 
-        #if auth not in AUTHS_SUPPORTED:
-            #raise BadUserConfigurationException(u"Auth '{}' not supported".format(auth))
-        
+
         self.url = url.rstrip(u"/")
-    
         self.auth = auth
         # implicitly_added is set to True only if the endpoint wasn't configured manually by the user through
         # a widget, but was instead implicitly defined as an endpoint to a wrapper kernel in the configuration
         # JSON file.
         self.implicitly_added = implicitly_added
-
+        
     def __eq__(self, other):
         if type(other) is not Endpoint:
             return False
